056671632c3b9f77e9508a7d4cfc99c27b59824f
sprime01 <sprime01@gmail.com>
2025-09-07 14:44:56 -0400

    chore(deangularize): remove Angular apps/libs and deps; clean Nx config and plugin peer deps

--- libs/miles/domain/src/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './lib/miles-domain/miles-domain.component';
-
-export * from './lib/entities/ticket';
-export * from './lib/infrastructure/ticket.data.service';
-
-export * from './lib/application/manage.facade';
--- libs/miles/feature-manage/README.md
+++
@@ -1,7 +0,0 @@
-# miles-feature-manage
-
-This library was generated with [Nx](https://nx.dev).
-
-## Running unit tests
-
-Run `nx test miles-feature-manage` to execute the unit tests.
